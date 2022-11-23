dc413f653701c8e887d27973e3be67e8134db917
biplab990 <biplabuddin990@gmail.com>
2022-11-23 07:56:45 +0600

    custom login

---
+++ resources/views/auth/registration.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>custom Authentication</title>
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offser-4" style="margin-top: 20px">
+                <h3>Registration</h3>
+                <form action="{{ route('register-user') }}" method="POST">
+                    @if (Session::has('success'))
+                    <div alert alert-success>{{ Session::get('success') }}</div>
+                    @endif
+                    @if (Session::has('fail'))
+                    <div alert alert-danger>{{ Session::get('fail') }}</div>
+                    @endif
+                    @csrf
+                    <div class="form-gorup">
+                        <label for="name">Full Name:</label>
+                        <input type="text" class="form-control" name="name" value="{{ old('name') }}">
+                        <span class="text-danger">@error('name') {{ $message }} @enderror</span>
+                    </div>
+                    <div class="form-gorup">
+                        <label for="email">email:</label>
+                        <input type="email" class="form-control" name="email" value="{{ old('email') }}">
+                        <span class="text-danger">@error('email') {{ $message }} @enderror</span>
+                    </div>
+                    <div class="form-gorup">
+                        <label for="password">Password:</label>
+                        <input type="password" class="form-control" name="password" value="">
+                        <span class="text-danger">@error('password') {{ $message }} @enderror</span>
+                    </div>
+                    <div class="form-gorup mt-3">
+                        <button class="btn btn-black btn-primary" type="submit">Save</button>
+                    </div>
+
+                </form>
+            </div>
+        </div>
+    </div>
+
+</body>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</html>
